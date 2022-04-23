5be82014c5a731fe8e99f0bcf66917375057738d
Rizki Muh Pasinringi <rizkimuhpasinringi@gmail.com>
2022-04-23 16:56:53 +0800

    menambahkan footer

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+// alert("sebelummnya maaf soalnya aku dah update, jadi sekarang gak pure html css aja..sekarang ada javascriptnya dikit..eheee");
+
+// parallax
+// $(window).on("load", function () {
+//   $(".phome").addClass("phome-go");
+// });
