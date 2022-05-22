c57ff08b8f59f734c116e00e329a68d45727e0cd
Rizki Muh Pasinringi <rizkimuhpasinringi@gmail.com>
2022-05-22 22:46:01 +0800

    update responsive

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,8 @@
 // alert("sebelummnya maaf soalnya aku dah update, jadi sekarang gak pure html css aja..sekarang ada javascriptnya dikit..eheee");
 
 // parallax
-// $(window).on("load", function () {
-//   $(".phome").addClass("phome-go");
-// });
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
+
+  console.log(wScroll);
+});
